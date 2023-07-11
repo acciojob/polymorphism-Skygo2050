7276ee9a7171179b124c249d85bfd6c781c39082
skygo <aakashjha2050@gmail.com>
2023-07-11 16:09:25 +0530

    Don21

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,32 @@ package com.driver;
 
 public class Main {
 
+
+public static class Product{
+
+
+    public int product(int x, int y) {}
+
+    public int product(int x, int y, int z) {}
+
+    public double product(double x, double y) {}
+
+
+}
+
+
+    public static void main(String[] args) {
+
+
+        Product p  = new Product();
+        p.product();
+        p.product();
+        p.product();
+
+
+
+
+    }
+
+
 }
